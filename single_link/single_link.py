69fd327b8b57774eac6e825b0341267a641eed0a
jmemmen <janka.memmen@tu-berlin.de>
2024-08-07 10:22:46 +0200

    Detector types and results

--- single_link/single_link.py
+++ single_link/single_link.py
@@ -21,39 +21,8 @@ from warnings import warn
 from requsim.noise import NoiseChannel
 import math
 
-max_iter = 10**1
-
-ETA_P = 0.6  # preparation efficiency
-
-T_2 = 1  # dephasing time
-T_CUT = 1  # cutoff time
-
 C = 2 * 10**8 # speed of light in optical fibre
 L_ATT = 22 * 10**3  # attenuation length
-
-
-P_D_1 = 800e-9  # dark count probability per detector 1
-P_D_2 = 50e-9  # dark count probability per detector 2
-
-ETA_D_1 = 0.6  # detector efficiency for station 1
-ETA_D_2 = 0.1  # detector efficiency for station 2
-
-
-eta_1 = ETA_P * ETA_D_1
-eta_2 = ETA_P * ETA_D_2
-
-
-
-
-## debugging check for AttributeError: 'DiscardQubitEvent' object has no attribute 'req_objects_exist'
-# world = World()
-# station_A = Station(world, position=0, memory_noise=None)
-# station_B = Station(world, position=1, memory_noise=None)
-# qubit = Qubit(world, station_A)
-# event1 = DiscardQubitEvent(1,qubit)
-# #print(dir(event1))
-# print(hasattr(event1, 'req_objects_exist'))
-
 
 
 # convert dB to an efficiency
@@ -76,7 +45,7 @@ def construct_dephasing_noise_channel(dephasing_time):
 class SimpleProtocol(TwoLinkProtocol):
 
     def check(self, message=None):
-        
+
         left_pairs = self._get_left_pairs()
         num_left_pairs = len(left_pairs)
         num_left_pairs_scheduled = len(self._left_pairs_scheduled())
@@ -114,25 +83,44 @@ class SimpleProtocol(TwoLinkProtocol):
 
 
 # L_1, L_2 effective lengths of first and second link respectively
-def run(L_1, L_2, params, max_iter):
+def run(L_1, L_2, params, max_iter, detector_1, detector_2):
 
-    allowed_params = ["P_LINK", "T_P", "T_CUT", "F_INIT", "T_DP", "ETA_TOT_1", "ETA_TOT_2", "P_D_1", "P_D_2"]
+    detector_properties = {
+        "SNSDP": {"DE": 0.6, "DC": 800e-9},
+        "ID230": {"DE": 0.1, "DC": 50e-9},
+        "QUBE": {"DE": 0.6, "DC": 200e-9},
+    }
+
+    if detector_1 not in detector_properties or detector_2 not in detector_properties:
+        raise ValueError("Invalid detector type provided.")
+
+    props_1 = detector_properties[detector_1]
+    props_2 = detector_properties[detector_2]
+
+    allowed_params = ["P_LINK", "T_P", "T_CUT", "F_INIT", "T_DP", "EFF_LINK_1", "EFF_LINK_2"]
     for key in params:
         if key not in allowed_params:
             warn(f"params[{key}] is not a supported parameter and will be ignored.")
-    # unpack the parameters
+
+    # Unpack the parameters
     P_LINK = params.get("P_LINK", 1.0)
     T_P = params.get("T_P", 1e-6)  # preparation time
-    T_CUT = params.get("T_CUT", None) # cutoff time
+    T_CUT = params.get("T_CUT", None)  # cutoff time
     F_INIT = params.get("F_INIT", 1.0)  # initial fidelity of created pairs
-    ETA_TOT_1 = params.get("ETA_TOT_1", 0.36) # efficiency first link
-    ETA_TOT_2 = params.get("ETA_TOT_2", 0.06) # efficiency second link
-    P_D_1 = params.get("P_D_1", 800e-9) # dark count probability first detector
-    P_D_2 = params.get("P_D_2", 50e-9) # dark count probability second detector
+    EFF_LINK_1 = params.get("EFF_LINK_1", 1.0)
+    EFF_LINK_2 = params.get("EFF_LINK_2", 1.0)
+    ETA_1 = props_1["DE"] # detector efficiency 1
+    ETA_2 = props_2["DE"] # detector efficiency 2
+    P_D_1 = props_1["DC"] # dark count probability 1 (converted into probability with detection window 1e-9)
+    P_D_2 = props_2["DC"] # dark count probability 2 (converted into probability with detection window 1e-9)
     try:
         T_DP = params["T_DP"]  # dephasing time
     except KeyError as e:
         raise Exception('params["T_DP"] is a mandatory argument').with_traceback(e.__traceback__)
+
+    # combined efficiencies
+    ETA_TOT_1 = EFF_LINK_1*ETA_1
+    ETA_TOT_2 = EFF_LINK_2*ETA_2
 
     def time_distribution(source, ETA_TOT, P_D):
         comm_distance = np.max([np.abs(source.position - source.target_stations[0].position), np.abs(source.position - source.target_stations[1].position)])
@@ -162,7 +150,7 @@ def run(L_1, L_2, params, max_iter):
 
     world = World()
     station_A = Station(world, position=0, memory_noise=None, dark_count_probability=P_D_1)
-    station_central = Station(world, position=L_1, memory_noise=construct_dephasing_noise_channel(dephasing_time=T_2), memory_cutoff_time=T_CUT)
+    station_central = Station(world, position=L_1, memory_noise=construct_dephasing_noise_channel(dephasing_time=T_DP), memory_cutoff_time=T_CUT)
     station_B = Station(world, position=L_1+L_2, memory_noise=None, dark_count_probability=P_D_2)
     source_A = SchedulingSource(world, position=L_1, target_stations=[station_A, station_central], time_distribution=lambda source: time_distribution(source, ETA_TOT_1, P_D_1), state_generation=lambda source: state_generation(source, ETA_TOT_1, P_D_1))
     source_B = SchedulingSource(world, position=L_1, target_stations=[station_central, station_B], time_distribution=lambda source: time_distribution(source, ETA_TOT_2, P_D_2), state_generation=lambda source: state_generation(source, ETA_TOT_2, P_D_2))
@@ -170,20 +158,13 @@ def run(L_1, L_2, params, max_iter):
     protocol.setup()
 
     # filter to discard events from time to time, use when using cutoff times
-    filter_interval = int(1e4)
+    # filter_interval = int(1e4)
 
     # world.event_queue.add_recurring_filter(
-    # condition=lambda event: isinstance(event, DiscardQubitEvent) and
-    #                         event.type == "DiscardQubitEvent" and
+    # condition=lambda event: event.type == "DiscardQubitEvent" and
     #                         not event.req_objects_exist(),
     # filter_interval=filter_interval,
     # )
-
-    world.event_queue.add_recurring_filter(
-    condition=lambda event: event.type == "DiscardQubitEvent" and
-                            not event.req_objects_exist(),
-    filter_interval=filter_interval,
-    )
 
     # main loop
     current_message = None
@@ -194,49 +175,97 @@ def run(L_1, L_2, params, max_iter):
     return protocol
 
 
+from filelock import FileLock
+
 if __name__ == "__main__":
 
     # evaluation on local computer, simple loop
-    T_P_array = np.linspace(1e-6, 1e-6, 1)
-    loss_1_db = 24.4
-    loss_2_db = 23.4
-    eff_link_1 = convert_dB_to_efficiency(loss_1_db)
-    eff_link_2 = convert_dB_to_efficiency(loss_2_db)
-
-    for i in T_P_array:
-        print(f"preparation time: {i}")
-        p = run(L_1=90e3, L_2=91.2e3, params={"T_DP": 1, "T_P":i, "T_CUT": 0.1, "ETA_TOT_1": eff_link_1*eta_1, "ETA_TOT_2": eff_link_2*eta_2}, max_iter=5)
-        evaluation = standard_bipartite_evaluation(p.data)
-        print(evaluation)
-
-
-    # # evaluation on cluster, split into jobs
-    # task_index = int(os.environ["SLURM_ARRAY_TASK_ID"])
-    #
-    # T_P_array = np.linspace(1e-6, 1e-8, 5)
+    # T_P_array = np.linspace(1e-6, 1e-6, 1)
     # loss_1_db = 24.4
     # loss_2_db = 23.4
     # eff_link_1 = convert_dB_to_efficiency(loss_1_db)
     # eff_link_2 = convert_dB_to_efficiency(loss_2_db)
-    #
-    # output_file = "results/single_link_central.txt"
-    # with open(output_file, "a") as file:
-    #     # write parameters to the output file if it's the first task
-    #     if task_index == 0:
-    #         file.write("Parameters:\n")
-    #         file.write(f"Efficiency Link 1: efficiency link: {eff_link_1}, efficiency detector/source: {eta_1}\n")
-    #         file.write(f"Efficiency Link 2: efficiency link: {eff_link_2}, efficiency detector/source: {eta_2}\n")
-    #         file.write(f"T_DP = {T_2}, T_CUT = {T_CUT}, max_iter = {max_iter}, mode=sim\n")
-    #
-    #     prep_time = T_P_array[task_index]
-    #
-    #     p = run(L_1=90e3, L_2=91.2e3, params={"T_DP": 1, "T_P": prep_time, "T_CUT": T_CUT, "ETA_TOT_1": eff_link_1*eta_1, "ETA_TOT_2": eff_link_2*eta_2}, max_iter=max_iter, mode="sim")
+
+    # for i in T_P_array:
+    #     print(f"preparation time: {i}")
+    #     p = run(L_1=90e3, L_2=91.2e3, params={"T_DP": 1, "T_P":i, "T_CUT": 0.1, "ETA_TOT_1": eff_link_1*eta_1, "ETA_TOT_2": eff_link_2*eta_2}, max_iter=5)
     #     evaluation = standard_bipartite_evaluation(p.data)
-    #     fidelity = evaluation[1]
-    #     key_rate = evaluation[3]
-    #
-    #     # append the output to the file
-    #     file.write(f"Task {task_index}: T_P = {prep_time}, Fidelity = {fidelity}, Key rate per time = {key_rate}\n")
+    #     print(evaluation)
+
+
+    # evaluation on cluster, split into jobs, 1D plot
+    task_index = int(os.environ["SLURM_ARRAY_TASK_ID"])
+    max_iter = 10**4
+    T_CUT = 0.2 # cutoff time
+    prep_time = 1e-6
+    T_2_array = np.logspace(-3, 2, 80)
+    link_1_db_m = [12, 46e3]
+    link_2_db_m = [23, 89e3]
+    station_1 = "SNSDP"
+    station_2 = "ID230"
+    eff_link_1 = convert_dB_to_efficiency(link_1_db_m[0])
+    eff_link_2 = convert_dB_to_efficiency(link_2_db_m[0])
+
+    output_file = "results/Erf_Wal_Eit_T2_cut0.2.txt"
+    with open(output_file, "a") as file:
+        # write parameters to the output file if it's the first task
+
+        if task_index == 0:
+            file.write("Parameters:\n")
+            file.write(f"Station 1 type: {station_1}, Station 2 type: {station_2}\n")
+            file.write(f"Link 1 db: {link_1_db_m[0]}, Link 1 m: {link_1_db_m[1]}\n")
+            file.write(f"Link 2 db: {link_2_db_m[0]}, Link 2 m: {link_2_db_m[1]}\n")
+            file.write(f"T_CUT = {T_CUT}, T_P = {prep_time}, max_iter = {max_iter}, mode=sim\n")
+
+        deph_time = T_2_array[task_index]
+
+        p = run(L_1=link_1_db_m[1], L_2=link_2_db_m[1], params={"T_DP": deph_time, "T_P": prep_time, "T_CUT": T_CUT, "EFF_LINK_1": eff_link_1, "EFF_LINK_2": eff_link_2}, max_iter=max_iter, detector_1=station_1, detector_2=station_2)
+        evaluation = standard_bipartite_evaluation(p.data)
+        fidelity = evaluation[1]
+        key_rate = evaluation[3]
+
+        # append the output to the file
+        file.write(f"Task {task_index}: T_2 = {deph_time}, Fidelity = {fidelity}, Key rate per time = {key_rate}\n")
+
+
+    # # evaluation on cluster, split into jobs, 2D plot
+    # task_index = int(os.environ["SLURM_ARRAY_TASK_ID"])
+
+    # T_P_array = np.logspace(-6, -9, 10)
+    # T_2_array = np.logspace(-2, 1, 10)
+    # combinations = [(T_P_val, T_2_val) for T_P_val in T_P_array for T_2_val in T_2_array]
+    # T_P_val, T_2_val = combinations[task_index]
+
+
+
+    # loss_1_db = 24.4
+    # loss_2_db = 23.4
+    # eff_link_1 = convert_dB_to_efficiency(loss_1_db)
+    # eff_link_2 = convert_dB_to_efficiency(loss_2_db)
+
+    # output_file = "results/2D_nocut.txt"
+    # lock_file = output_file + ".lock"
+
+
+    # with FileLock(lock_file):
+    #     with open(output_file, "a") as file:
+    #         # write parameters to the output file if it's the first task
+    #         if task_index == 0:
+    #             file.write("Parameters:\n")
+    #             file.write(f"Efficiency Link 1: efficiency link: {eff_link_1}, efficiency detector/source: {eta_1}\n")
+    #             file.write(f"Efficiency Link 2: efficiency link: {eff_link_2}, efficiency detector/source: {eta_2}\n")
+    #             file.write(f"T_CUT = {T_CUT}, max_iter = {max_iter}, mode=sim\n")
+
+    #         p = run(L_1=90e3, L_2=91.2e3, params={"T_DP": T_2_val, "T_P": T_P_val, "T_CUT": T_CUT, "ETA_TOT_1": eff_link_1*eta_1, "ETA_TOT_2": eff_link_2*eta_2}, max_iter=max_iter)
+    #         evaluation = standard_bipartite_evaluation(p.data)
+    #         fidelity = evaluation[1]
+    #         key_rate = evaluation[3]
+
+    #         # append the output to the file
+    #         file.write(f"Task {task_index}: T_P = {T_P_val}, T_2 = {T_2_val}, Fidelity = {fidelity}, Key rate per time = {key_rate}\n")
+
+
+
 
 
 
